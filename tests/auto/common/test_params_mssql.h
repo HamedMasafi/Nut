8057691e0d8897f2f222689f673d86a584dbc945
Hamed Masafi <hamed.masfi@gmail.com>
2021-12-09 13:15:28 +0330

    mssql

--- tests/auto/common/test_params_mssql.h
+++ tests/auto/common/test_params_mssql.h
@@ -5,9 +5,9 @@
 #undef DATABASE
 #endif
 
-#define DATABASE QLatin1String("DRIVER={SQL SERVER};SERVER=.;DATABASE=;" DBNAME)
+#define DATABASE QLatin1String("DRIVER={SQL SERVER};SERVER=.;UID=sa;PWD=NUT_SA_PASS;DATABASE=" DBNAME)
 
 #define DRIVER QStringLiteral("QODBC")
 #define HOST QLatin1String("localhost")
-#define USERNAME QLatin1String()
-#define PASSWORD QLatin1String()
+#define USERNAME QLatin1String("sa")
+#define PASSWORD QLatin1String("NUT_SA_PASS")
