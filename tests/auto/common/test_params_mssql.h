2bb72704e72d8ee55883bb181b674ff428e13757
Hamed Masafi <hamed.masafi@gmail.com>
2021-12-11 21:09:34 +0330

    Change sql server address to localhost

--- tests/auto/common/test_params_mssql.h
+++ tests/auto/common/test_params_mssql.h
@@ -5,12 +5,12 @@
 #endif
 
 #define DATABASE                                                                                   \
-    QLatin1String("DRIVER={SQL SERVER};SERVER=.;UID=sa;PWD=NUT_sa_PASS_1_???;DATABASE=")           \
+    QLatin1String("DRIVER={SQL SERVER};SERVER=localhost;UID=sa;PWD=NUT_sa_PASS_1_???;DATABASE=")           \
         + QStringLiteral("nut_test_%1_db")                                                         \
               .arg(QString::fromUtf8(metaObject()->className()))                                   \
               .toLower()
 
 #define DRIVER QStringLiteral("QODBC")
 #define HOST QLatin1String("localhost")
-#define USERNAME QLatin1String("sa")
-#define PASSWORD QLatin1String("NUT_sa_PASS_1_???")
+#define USERNAME QLatin1String("")
+#define PASSWORD QLatin1String("")
