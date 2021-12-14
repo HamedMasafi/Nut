6bf9674c09d81f0406cadb205117dbe9d60f6d43
Hamed Masafi <hamed.masfi@gmail.com>
2021-12-14 18:46:27 +0330

    switch to sql server local db

--- tests/auto/common/test_params_mssql.h
+++ tests/auto/common/test_params_mssql.h
@@ -5,10 +5,13 @@
 #endif
 
 #define DATABASE                                                                                   \
-    QStringLiteral("DRIVER={SQL Server Native Client 11.0};Server=.;Database=nut_test_%1_db")      \
+    QStringLiteral("DRIVER={SQL Server Native Client 11.0};"                                       \
+                   "Server=(LocalDB)\\MSSQLLocalDB;"                                                \
+                   "Integrated Security=true;"                                                     \
+                   "Database=nut_test_%1_db")                                                      \
         .arg(QString::fromUtf8(metaObject()->className()).toLower())
 
 #define DRIVER QStringLiteral("QODBC")
 #define HOST QStringLiteral("localhost")
-#define USERNAME QStringLiteral("sa")
-#define PASSWORD QStringLiteral("NUT_sa_PASS_1_???")
+#define USERNAME QLatin1String("")
+#define PASSWORD QLatin1String("")
