7506dbb8ecadf9cd7eb69fed88fd690a7ec78c98
Hamed Masafi <hamed.masfi@gmail.com>
2022-01-22 17:55:22 +0330

    test for new sql server connection string

--- tests/auto/common/test_params_mssql.h
+++ tests/auto/common/test_params_mssql.h
@@ -5,10 +5,10 @@
 #endif
 
 #define DATABASE                                                                                   \
-    QStringLiteral("DRIVER={SQL Server Native Client 11.0};Server=.;Database=nut_test_%1_db")      \
+    QStringLiteral("DRIVER={SQL Server Native Client 11.0};Server=localhost;Database=nut_test_%1_db;UID=sa;PWD=NUT_sa_PASS_1_???;")      \
         .arg(QString::fromUtf8(metaObject()->className()).toLower())
 
 #define DRIVER QStringLiteral("QODBC")
-#define HOST QStringLiteral("localhost")
-#define USERNAME QStringLiteral("sa")
-#define PASSWORD QStringLiteral("NUT_sa_PASS_1_???")
+#define HOST QLatin1String("")
+#define USERNAME QLatin1String("")
+#define PASSWORD QLatin1String("")
