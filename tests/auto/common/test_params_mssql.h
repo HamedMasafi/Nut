8c2fa7c6cc2a6273e8d7bcd89b2294898ae15675
Hamed Masafi <hamed.masfi@gmail.com>
2022-01-22 18:30:36 +0330

    fix: change DATABASE to uppercase

--- tests/auto/common/test_params_mssql.h
+++ tests/auto/common/test_params_mssql.h
@@ -5,7 +5,7 @@
 #endif
 
 #define DATABASE                                                                                   \
-    QStringLiteral("DRIVER={SQL Server Native Client 11.0};Server=localhost;Database=nut_test_%1_db;UID=sa;PWD=NUT_sa_PASS_1_???;")      \
+    QStringLiteral("DRIVER={SQL Server Native Client 11.0};Server=localhost;DATABASE=nut_test_%1_db;UID=sa;PWD=NUT_sa_PASS_1_???;")      \
         .arg(QString::fromUtf8(metaObject()->className()).toLower())
 
 #define DRIVER QStringLiteral("QODBC")
